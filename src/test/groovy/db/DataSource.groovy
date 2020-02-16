a9eb7c070df8a04eb7a69e3d4b967cdf2b011f39
ZhangJin233 <jane.zhangjin@outlook.com>
2020-02-16 13:50:20 +0800

    Add mysql database

---
+++ src/test/groovy/db/DataSource.groovy
@@ -0,0 +1,30 @@
+/**
+ *
+ * @author Jane* @date 2020-02-14 11:16 AM
+ * 连接mysql数据库固定写法
+ *
+ * */
+package db
+
+
+import groovy.sql.Sql
+
+
+class DataSource {
+    Sql sql
+
+    Sql getSql() {
+        if (!sql) {
+            def mysqlDB = [
+                    driver  : 'com.mysql.jdbc.Driver',
+                    url     : 'jdbc:mysql://localhost:3306/apitestdb',
+                    user    : 'root',
+                    password: 'root12345'
+            ]
+
+            sql = Sql.newInstance(mysqlDB.url, mysqlDB.user, mysqlDB.password, mysqlDB.driver)
+            println "DB connection ready"
+        }
+        sql
+    }
+}
