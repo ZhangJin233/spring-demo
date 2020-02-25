e1edab195123f3f18c2609ef80a92ed6d2aa0f79
ZhangJin233 <jane.zhangjin@outlook.com>
2020-02-25 16:15:10 +0800

    Add verify header、cookie by schema

--- src/test/groovy/ResponseTest/ResumeClient.groovy
+++ src/test/groovy/ResponseTest/ResumeClient.groovy
@@ -1,5 +1,6 @@
 package ResponseTest
 
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath
 import static io.restassured.RestAssured.given
 
 /**
@@ -18,15 +19,15 @@ class ResumeClient {
         res
     }
 
-    void getResumeDetails2(){
+    void getResumeDetails2() {
         given().port(9090)
                 .when()
                 .get("/api/getResume")
                 .then().assertThat().statusCode(200)
-                .body("name",equals("TOM"))
+                .body("name", equals("TOM"))
     }
 
-    def getResumeDetails3(){
+    def getResumeDetails3() {
         given().port(9090)
                 .when()
                 .get("/api/getResume")
@@ -35,12 +36,32 @@ class ResumeClient {
                 .response().path("contacts.phone")
     }
 
-    def getResumeDetailHeader(){
+    def getResumeDetailHeader() {
         given().baseUri("http://localhost:9090")
                 .when()
                 .get("/api/getResume")
                 .extract()
                 .response().getHeader("content-type")
+    }
+
+    def getResumeSchemaValidate(filePath) {
+        given().baseUri("http://localhost:9090")
+                .when()
+                .get("/api/getResume")
+                .then().assertThat()
+        //在调用接口时通过matchsJsonSchemaInClassPath校验接口的response schema
+                .body(matchesJsonSchemaInClasspath((String) filePath))
+    }
+
+    def getResume2() {
+        def payLoad = given().baseUri("http://localhost:9090")
+                .when()
+                .get("/api/getResume2")
+                .then().assertThat().statusCode(200)
+                .extract()
+                .response()
+                .path("payLoad")
+        payLoad
     }
 
 }
--- src/test/groovy/ResponseTest/TestCase.groovy
+++ src/test/groovy/ResponseTest/TestCase.groovy
@@ -1,6 +1,8 @@
 package ResponseTest
 
 import spock.lang.Specification
+import static org.hamcrest.MatcherAssert.assertThat
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath
 
 /**
  *
@@ -29,7 +31,7 @@ class TestCase extends Specification {
         "USA"   | ""
     }
 
-    def "get contact from resume"(){
+    def "get contact from resume"() {
         given: "no given"
         when: "call the get resume api"
         def res = resumeClient.getResumeDetails()
@@ -37,23 +39,44 @@ class TestCase extends Specification {
         println resumeService.getContactPhone(res)
     }
 
-    def "println language skill if person with it"(){
+    def "println language skill if person with it"() {
         given: "no given"
         when: "call the get resume api"
         def res = resumeClient.getResumeDetails()
         then: "println out contacts info"
-        println resumeService.printIfPersonWIthSpecialSkill(res,language)
+        println resumeService.printIfPersonWIthSpecialSkill(res, language)
         where:
         language | placeHolder
         "Java"   | ""
     }
 
-    def "get all working experience"(){
+    def "get all working experience"() {
         given: "no given"
         when: "call the get resume api"
         def res = resumeClient.getResumeDetails()
         then: "println out contacts info"
         println resumeService.printWorkingDetails(res)
 
+    }
+
+    def "call the api"() {
+        given: "no given"
+        when: "call the get resume api"
+        resumeClient.getResumeSchemaValidate(filePath)
+        then: "no then"
+        where:
+        filePath                                      | placeHolder
+        "com.spring.demo/schema/getResumeSchema.json" | ""
+    }
+
+    def "validate schema of getResume2 api"() {
+        given: "no given"
+        when: "call the get resume2 api"
+        def payLoad = resumeClient.getResume2()
+        then: "check the schema"
+        assertThat(payLoad, matchesJsonSchemaInClasspath(filePath))
+        where:
+        filePath                                                            | placeHolder
+        "./src/test/resources/com.spring.demo/schema/getResumeSchema2.json" | ""
     }
 }
